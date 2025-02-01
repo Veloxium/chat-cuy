878832e3c499c13bf811df45b93883f7238155e6
Gylmynnn <muhamadgilman999@gmail.com>
2025-02-01 17:32:43 +0800

    core : database postgres connection

---
+++ be/database/db.go
@@ -0,0 +1,30 @@
+package database
+
+import (
+	"database/sql"
+	_ "github.com/lib/pq"
+)
+
+type Database struct {
+	db *sql.DB
+}
+
+func NewDatabaseConn() (*Database, error) {
+	driverName := "postgres"
+	dataSource := "postgresql://root:24434@localhost:5432/go-websocket-sesat?sslmode=disable"
+	db, err := sql.Open(driverName, dataSource)
+	if err != nil {
+		return nil, err
+	}
+	return &Database{
+		db: db,
+	}, nil
+}
+
+func (d *Database) Close() {
+	d.db.Close()
+}
+
+func (d *Database) GetDB() *sql.DB {
+	return d.db
+}
