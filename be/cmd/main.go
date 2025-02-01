1cb805672b651102d0ce7091959ce2756024f4fa
Gylmynnn <muhamadgilman999@gmail.com>
2025-02-01 17:32:19 +0800

    core : root application

---
+++ be/cmd/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/Gylmynnn/websocket-sesat/database"
+	"github.com/Gylmynnn/websocket-sesat/internal/user"
+	"github.com/Gylmynnn/websocket-sesat/internal/websocket"
+	"github.com/Gylmynnn/websocket-sesat/router"
+)
+
+func main() {
+	dbConnection, err := database.NewDatabaseConn()
+	if err != nil {
+		log.Fatalf("error when initial database %s", err)
+	}
+
+	userRepository := user.NewRepository(dbConnection.GetDB())
+	userService := user.NewService(userRepository)
+	userHandler := user.NewHundler(userService)
+
+	hub := websocket.NewHub()
+	wsHandler := websocket.NewHandler(hub)
+   go hub.Run()
+
+	router.InitRouter(userHandler, wsHandler)
+	router.Start("0.0.0.0:8080")
+	fmt.Println("server running on port : 8080")
+}
