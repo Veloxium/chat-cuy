12afa6c2b8d57d6a6de4d13468acfb92eecf79c8
Gylmynnn <muhamadgilman999@gmail.com>
2025-02-01 17:36:48 +0800

    core : project router

---
+++ be/router/router.go
@@ -0,0 +1,25 @@
+package router
+
+import (
+	"github.com/Gylmynnn/websocket-sesat/internal/user"
+	"github.com/Gylmynnn/websocket-sesat/internal/websocket"
+	"github.com/gin-gonic/gin"
+)
+
+var r *gin.Engine
+
+func InitRouter(userHandler *user.Handler, wsHandler *websocket.Handler) {
+	r = gin.Default()
+	r.POST("/signup", userHandler.CreateUser)
+	r.POST("/login", userHandler.Login)
+	r.GET("/logout", userHandler.Logout)
+
+	r.POST("/ws/createRoom", wsHandler.CreateRoom)
+	r.GET("/ws/joinRoom/:roomId", wsHandler.JoinRoom)
+	r.GET("/ws/getRooms", wsHandler.GetRooms)
+	r.GET("/ws/getClients/:roomId", wsHandler.GetClient)
+}
+
+func Start(addr string) error {
+	return r.Run(addr)
+}
