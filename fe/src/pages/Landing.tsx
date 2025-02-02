1d775b5fb540e0f4141ee6ea9fca8c4c9647ebac
Veloxium <veloxium.09@gmail.com>
2025-02-02 14:34:54 +0800

    feat: create IndexLayout component

--- fe/src/pages/Landing.tsx
+++ fe/src/pages/Landing.tsx
@@ -1,26 +1,32 @@
 import { Button } from "@/components/ui/button";
+import IndexLayout from "@/layout/IndexLayout";
 import { toast } from "sonner";
 
 function LandingPage() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen gap-8">
-      <p className="text-5xl font-forta">LandingPage</p>
-      <Button
-        variant="default"
-        className=""
-        onClick={() =>
-          toast("Event has been created", {
-            description: "Sunday, December 03, 2023 at 9:00 AM",
-            action: {
-              label: "Undo",
-              onClick: () => console.log("Undo"),
-            },
-          })
-        }
-      >
-        Show Toast
-      </Button>
-    </div>
+    <IndexLayout>
+      <div className="flex flex-col items-center justify-center h-screen gap-8">
+        <div className="text-center">
+          <p className="text-2xl font-forta">Welcome to</p>
+          <p className="text-5xl font-forta">Chat Cuy😲</p>
+        </div>
+        <Button
+          variant="default"
+          className=""
+          onClick={() =>
+            toast("Event has been created", {
+              description: "Sunday, December 03, 2023 at 9:00 AM",
+              action: {
+                label: "Undo",
+                onClick: () => console.log("Undo"),
+              },
+            })
+          }
+        >
+          Show Toast
+        </Button>
+      </div>
+    </IndexLayout>
   );
 }
 
