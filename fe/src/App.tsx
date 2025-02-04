58716c5cc0e61621e2e574482856a8707a8b1fe2
Veloxium <veloxium.09@gmail.com>
2025-02-04 14:59:29 +0800

    feat: enhance LandingPage with new UI elements and input fields

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -3,6 +3,7 @@ import "./App.css";
 import LandingPage from "./pages/Landing";
 import { dynamicTitle } from "./utils/dynamicTitle";
 import { Toaster } from "./components/ui/sonner";
+import HomePage from "./pages/Home";
 
 function App() {
   dynamicTitle();
@@ -13,6 +14,7 @@ function App() {
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<LandingPage />} />
+            <Route path="/home" element={<HomePage />} />
           </Routes>
         </BrowserRouter>
       </div>
