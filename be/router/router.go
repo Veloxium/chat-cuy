cc31092b5aa05492f69853d39b4c919eb767829c
Gylmynnn <muhamadgilman999@gmail.com>
2025-03-02 23:11:54 +0800

    feat : add middleware to protected endpoint

--- be/router/router.go
+++ be/router/router.go
@@ -1,22 +1,27 @@
 package router
 
 import (
+	"time"
+
+	"github.com/Gylmynnn/websocket-sesat/internal/contact"
 	"github.com/Gylmynnn/websocket-sesat/internal/user"
 	"github.com/Gylmynnn/websocket-sesat/internal/websocket"
+	"github.com/Gylmynnn/websocket-sesat/protected"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
-	"time"
 )
 
 var app *gin.Engine
 
-func InitRouter(userHandler *user.Handler, wsHandler *websocket.Handler) {
+func InitRouter(userHandler *user.Handler, wsHandler *websocket.Handler, contactHandler *contact.Handler) {
 	app = gin.Default()
+
+	app.Use(gin.Logger())
 
 	app.Use(cors.New(cors.Config{
 		AllowOrigins:     []string{"http://localhost:5173"},
-		AllowMethods:     []string{"GET", "POST"},
-		AllowHeaders:     []string{"Content-Type"},
+		AllowMethods:     []string{"GET", "POST", "PUT"},
+		AllowHeaders:     []string{"Content-Type", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
 		AllowCredentials: true,
 		AllowOriginFunc: func(origin string) bool {
@@ -30,10 +35,15 @@ func InitRouter(userHandler *user.Handler, wsHandler *websocket.Handler) {
 	app.POST("/withfacebook", userHandler.LoginWithFacebook)
 	app.GET("/logout", userHandler.Logout)
 
-	app.POST("/ws/createRoom", wsHandler.CreateRoom)
-	app.GET("/ws/joinRoom/:roomId", wsHandler.JoinRoom)
-	app.GET("/ws/getRooms", wsHandler.GetRooms)
-	app.GET("/ws/getClients/:roomId", wsHandler.GetClient)
+	authApp := app.Group("/")
+	authApp.Use(protected.JWTAuthMiddleware())
+
+	authApp.POST("/contact", contactHandler.AddContact)
+
+	authApp.POST("/ws/createRoom", wsHandler.CreateRoom)
+	authApp.GET("/ws/joinRoom/:roomId", wsHandler.JoinRoom)
+	authApp.GET("/ws/getRooms", wsHandler.GetRooms)
+	authApp.GET("/ws/getClients/:roomId", wsHandler.GetClient)
 }
 
 func Start(addr string) error {
