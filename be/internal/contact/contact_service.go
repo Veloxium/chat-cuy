91d66f6b7e1c2db162fc40a06dd033fc01909739
Gylmynnn <muhamadgilman999@gmail.com>
2025-03-17 17:27:17 +0800

    feat : implement delete, get contact service

--- be/internal/contact/contact_service.go
+++ be/internal/contact/contact_service.go
@@ -17,6 +17,34 @@ func NewService(repository Repository) Service {
 	}
 }
 
+func (s *service) GetContactByID(c context.Context, contactID int64) (*GetContactsRes, error) {
+
+}
+
+func (s *service) GetAllContacts(c context.Context) ([]GetContactsRes, error) {
+	ctx, cancle := context.WithTimeout(c, s.timeout)
+	defer cancle()
+
+	contacts, err := s.Repository.GetAllContacts(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return contacts, nil
+
+}
+
+func (s *service) DeleteContact(c context.Context, contactID int64) error {
+	ctx, cancle := context.WithTimeout(c, s.timeout)
+	defer cancle()
+
+	err := s.Repository.DeleteContact(ctx, contactID)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
 func (s *service) AddContact(c context.Context, req *CreateContactReq) (*CreateContactRes, error) {
 	ctx, cancle := context.WithTimeout(c, s.timeout)
 	defer cancle()
@@ -38,5 +66,5 @@ func (s *service) AddContact(c context.Context, req *CreateContactReq) (*CreateC
 		Username:  data.Username,
 		CreatedAt: data.CreatedAt,
 	}
-   return result, nil
+	return result, nil
 }
