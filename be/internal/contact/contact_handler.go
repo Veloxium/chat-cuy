85a26fd85bc39725cddb054ca1b7a083186154be
Gylmynnn <muhamadgilman999@gmail.com>
2025-03-17 17:25:09 +0800

    feat : implement delete contact handler

--- be/internal/contact/contact_handler.go
+++ be/internal/contact/contact_handler.go
@@ -3,10 +3,9 @@ package contact
 import (
 	"net/http"
 
-	"github.com/Gylmynnn/websocket-sesat/pkg/utils"
+	"github.com/Gylmynnn/websocket-sesat/utils"
 	"github.com/gin-gonic/gin"
 )
-
 
 type Handler struct {
 	Service
@@ -18,8 +17,59 @@ func NewHundler(s Service) *Handler {
 	}
 }
 
+func (h *Handler) DeleteContact(c *gin.Context) {
+	contactID, err := utils.ParseIDParam(c, "id")
+	if err != nil {
+		c.JSON(http.StatusBadRequest, utils.ResFormatter{
+			Success:    false,
+			StatusCode: http.StatusBadRequest,
+			Message:    "error :" + err.Error(),
+			Data:       nil,
+		})
+		c.Abort()
+		return
+	}
 
-func (h *Handler) AddContact (c *gin.Context) {
+	err = h.Service.DeleteContact(c.Request.Context(), contactID)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, utils.ResFormatter{
+			Success:    false,
+			StatusCode: http.StatusInternalServerError,
+			Message:    "error :" + err.Error(),
+			Data:       nil,
+		})
+		c.Abort()
+		return
+	}
+	c.JSON(http.StatusOK, utils.ResFormatter{
+		Success:    true,
+		StatusCode: http.StatusOK,
+		Message:    "contact deleted successfully",
+		Data:       nil,
+	})
+}
+
+func (h *Handler) GetAllContacts(c *gin.Context) {
+	res, err := h.Service.GetAllContacts(c.Request.Context())
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, utils.ResFormatter{
+			Success:    false,
+			StatusCode: http.StatusInternalServerError,
+			Message:    "error :" + err.Error(),
+			Data:       nil,
+		})
+		return
+
+	}
+	c.JSON(http.StatusOK, utils.ResFormatter{
+		Success:    true,
+		StatusCode: http.StatusOK,
+		Message:    "get contacts successfully",
+		Data:       res,
+	})
+}
+
+func (h *Handler) AddContact(c *gin.Context) {
 	var contact CreateContactReq
 	if err := c.ShouldBindJSON(&contact); err != nil {
 		c.JSON(http.StatusBadRequest, utils.ResFormatter{
