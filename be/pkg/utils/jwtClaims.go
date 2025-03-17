c41faed28ff786440aa2bb2ec5bae3bac922f137
Gylmynnn <muhamadgilman999@gmail.com>
2025-03-17 17:46:44 +0800

    ref : move folder structure

--- be/pkg/utils/jwtClaims.go
+++
@@ -1,9 +0,0 @@
-package utils
-
-import "github.com/golang-jwt/jwt/v5"
-
-type MyJWTClaims struct {
-	ID       string `json:"id"`
-	Username string `json:"username"`
-	jwt.RegisteredClaims
-}
--- be/pkg/utils/resFormatter.go
+++
@@ -1,8 +0,0 @@
-package utils
-
-type ResFormatter struct {
-	Success    bool   `json:"success"`
-	StatusCode int    `json:"statusCode"`
-	Message    string `json:"message"`
-	Data       any    `json:"data"`
-}
