a4c044968b0b0f07f5177501bb7d90f6fb901c1e
Gylmynnn <muhamadgilman999@gmail.com>
2025-03-25 19:35:40 +0800

    delete unused

--- be/database/migration/20250227101110_add_contact_table.up.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE "contacts" (
-    "id" bigserial PRIMARY KEY,
-    "user_id" bigint NOT NULL,
-    "username" varchar NOT NULL,
-    "avatar" varchar NOT NULL,
-    "created_at" TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    "deleted_at" TIMESTAMP NULL,
-    CONSTRAINT "fk_contacts_users" FOREIGN KEY ("user_id") REFERENCES "users" ("id") ON DELETE CASCADE
-);
