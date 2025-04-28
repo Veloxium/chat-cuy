ee2534f887b11c92f6a769c3e79808f18f136e93
Gylmynnn <muhamadgilman999@gmail.com>
2025-04-28 19:02:05 +0800

    ref : return firebase error initialized

--- be/database/firebase.go
+++ be/database/firebase.go
@@ -2,26 +2,16 @@ package database
 
 import (
 	"context"
-	"log"
-
+	firebase "firebase.google.com/go/v4"
 	"firebase.google.com/go/v4/auth"
 	"google.golang.org/api/option"
-
-	firebase "firebase.google.com/go/v4"
 )
 
 var AuthClient *auth.Client
 
-func InitFirebase() {
+func InitFirebase() error {
 	opt := option.WithCredentialsFile("secret/admin-sdk.json")
-
 	app, err := firebase.NewApp(context.Background(), nil, opt)
-	if err != nil {
-		log.Fatalf("failed to inisialize firebase app : %v", err)
-	}
-
 	AuthClient, err = app.Auth(context.Background())
-	if err != nil {
-		log.Fatalf("failed to inisialize firebase auth client : %v", err)
-	}
+	return err
 }
